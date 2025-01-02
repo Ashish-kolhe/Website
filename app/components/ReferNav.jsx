4f1f469baaa166f5e80ca19f627ab21dea76fd82
Ashish-kolhe <ashishkolhe2005@gamil.com>
2025-01-02 14:18:46 +0530

    Thirteen

---
+++ app/components/ReferNav.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const ReferNav = () => {
+  return (
+    <div>
+    <div className="navbar bg-white py-4">
+<div className="navbar-start">
+<div className="dropdown">
+   
+  <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+  <a href="/">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" height="20" width="22.5"><path fill="#000000" d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
+  </a>
+  </div>
+ 
+</div>
+</div>
+<div className="navbar-center">
+
+<a className="font-black text-xl text-gray-900" >Refer & Earn</a>
+
+</div>
+<div className="navbar-end mr-2">
+
+<a href="https://refer-coddeit.vercel.app/">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="20" width="22.5"><path fill="#000000" d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64L0 400c0 44.2 35.8 80 80 80l400 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 416c-8.8 0-16-7.2-16-16L64 64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z"/></svg>
+  </a>
+</div>
+</div>
+</div>
+  )
+}
+
+export default ReferNav
---
+++ app/refer/page.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReferNav from '../components/ReferNav'
+
+const page = () => {
+  return (
+    <div>
+    <ReferNav/>
+  <div className=" flex-row justify-items-center mt-4 ">
+  <img className='w-24' src="./rupee.gif" alt="" />
+  <h1 className="font-black text-sm text-gray-900 mt-2">Be the First to Refer and Earn!</h1>
+  <div className="overflow-x-auto mt-8 border-2 border-secondary rounded-2xl">
+<table className="table text-center bg-white">
+ 
+  <thead>
+    <tr className="text-secondary">
+     
+      <th>Level</th>
+      <th>Range</th>
+      <th>₹ Per Referral</th>
+      <th>Earnings</th>
+    </tr>
+  </thead>
+  <tbody className="text-sm font-bold">
+    {/* row 1 */}
+    <tr>
+      
+      <td>Starter</td>
+      <td>0-9</td>
+      <td>₹10</td>
+      <td>₹90</td>
+    </tr>
+    {/* row 2 */}
+    <tr className="text-info">
+      
+    <td>Level 1</td>
+      <td>10-24</td>
+      <td>₹13</td>
+      <td>₹195</td>
+    </tr>
+    {/* row 3 */}
+    <tr className="text-success">
+      
+    <td>Level 2</td>
+      <td>25-49</td>
+      <td>₹15</td>
+      <td>₹375</td>
+    </tr>
+    <tr className="text-red-500">
+      
+      <td>Level 3</td>
+        <td>50-99</td>
+        <td>₹17</td>
+        <td>₹850</td>
+      </tr>
+      
+  </tbody>
+</table>
+</div>
+<h1 className="font-black text-sm text-gray-800 mt-3">Total Estimated Earnings for <span className="text-green-500 ">100</span> Referrals:</h1>
+<h1 className="text-3xl font-black text-secondary mt-1 mb-8">₹1500</h1>
+<h1 className="text-xs font-black text-gray-700 mt-1 mb-20">If you are already registered, <a className='text-blue-600 underline' href="https://refer-coddeit.vercel.app/">Click Here</a> 👈</h1>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSceGYvXY7pPzjAvZlYTqHpOPMzeGlNJfMkO2DkbhXuGqxOxdQ/viewform?embedded=true" width="400" height="1400"  >Loading…</iframe>
+    </div>
+    </div>
+  )
+}
+
+export default page
